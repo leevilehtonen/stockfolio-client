5820e75103068adfd9effcdb2abd866c649e6f61
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-05-11 02:53:31 +0300

    config created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css'
+import './styles/index.css';
 
 ReactDOM.render(
   <App />,
