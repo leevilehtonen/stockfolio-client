2956fe47c337c5bfbdceda9774b9548c4475d469
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-05-24 15:58:25 +0300

    travis

--- src/components/Quote/QuoteView.js
+++ src/components/Quote/QuoteView.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Modal, ModalBody, ModalHeader, ModalFooter, ButtonGroup, Button, Progress, Table } from 'reactstrap';
 import { connect } from 'react-redux';
 import { Line, defaults } from 'react-chartjs-2';
